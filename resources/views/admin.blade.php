0c4beb307ce240345f493fa56e8ad1483ea5b943
omgitusufiyya <omgitusufiyya@gmail.com>
2026-01-15 17:32:02 +0800

    Update

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -87,7 +87,7 @@
     </table>
     @endif
 
-    <a href="{{ route('mainpage') }}" class="btn btn-secondary mt-3">Back to Home</a>
+    <a href="{{ route('mainpage') }}" class="btn btn-secondary mt-3">Logout</a>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
